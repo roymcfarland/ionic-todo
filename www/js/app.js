69700212b88179360261a1be1f09325181489179
Roy McFarland <roy.mcfarland@gmail.com>
2015-05-21 11:46:52 -0600

    reconfig ng-app; insert left side menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+
+/*
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -17,3 +19,6 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+*/
+
+angular.module('todo', ['ionic'])
